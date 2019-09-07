d2f87cca44d241879736c421c509e5059cd01fd1
Apolo Rossi <apolorossi@gmail.com>
2019-09-06 23:15:49 -0300

    Feat: Created adapter and recycler view for movies.

--- app/src/main/java/com/apolo/findmovies/presentation/home/view/HomeActivity.kt
+++ app/src/main/java/com/apolo/findmovies/presentation/home/view/HomeActivity.kt
@@ -3,12 +3,16 @@ package com.apolo.findmovies.presentation.home.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.apolo.findmovies.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+//        movies_list.adapter =
 
 
     }
