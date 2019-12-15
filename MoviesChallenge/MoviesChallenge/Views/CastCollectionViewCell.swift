fdac23c218c24634b1f25ed05c3331b4a6a8a765
leobortolotti <leobortolotti@me.com>
2019-12-15 01:03:33 -0300

    Projeto finalizado

---
+++ MoviesChallenge/MoviesChallenge/Views/CastCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CastCollectionViewCell.swift
+//  MoviesChallenge
+//
+//  Created by Leonardo Bortolotti on 15/12/19.
+//  Copyright © 2019 Leonardo Bortolotti. All rights reserved.
+//
+
+import UIKit
+
+class CastCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
